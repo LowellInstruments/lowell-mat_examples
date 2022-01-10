6d84dc129025c68b12dd0512bb144cc586981def
kaz <kaz@pop-os.localdomain>
2022-01-10 15:42:54 -0500

    typo led

--- ble/bleak/do2_dummy/led.py
+++ ble/bleak/do2_dummy/led.py
@@ -1,6 +1,6 @@
-from ble.bleak.do2.led import leds
+from ble.bleak.do2.led import led
 from mat.ble.bleak_beta.logger_do2_dummy import LoggerDO2Dummy
 
 
 if __name__ == "__main__":
-    leds(LoggerDO2Dummy)
+    led(LoggerDO2Dummy)
