468aca085584fbbcc7b0b646c2c5a0d352447d1c
kaz <kaz>
2022-03-06 05:57:01 -0500

    examples

--- ble/bluepy/cc26x2r/get_sensor_oxygen.py
+++ ble/bluepy/cc26x2r/get_sensor_oxygen.py
@@ -3,23 +3,28 @@ from mat.ble.bluepy.cc26x2r_logger_controller import LoggerControllerCC26X2R
 from _macs import get_mac
 
 
-def measure_oxygen():
+def measure_oxygen(pre_stp=False):
 
     mac = get_mac()
     lc = LoggerControllerCC26X2R(mac)
 
     if lc.open():
-        while 1:
+        for i in range(1):
+
+            if pre_stp:
+                rv = lc.ble_cmd_stp()
+                print('stop', rv)
+
             rv = lc.ble_cmd_gdo()
             print('> DO saturation:  {}.{} mg/l'.format(rv[0][:2], rv[0][2:]))
             print('> DO percentage:  {}.{} %'.format(rv[1][:2], rv[1][2:]))
             print('> DO temperature: {}.{} C'.format(rv[2][:2], rv[2][2:]))
             print('\n')
-            time.sleep(10)
+            time.sleep(5)
     else:
         print('{} connection error'.format(__name__))
     lc.close()
 
 
 if __name__ == '__main__':
-    measure_oxygen()
+    measure_oxygen(pre_stp=True)
