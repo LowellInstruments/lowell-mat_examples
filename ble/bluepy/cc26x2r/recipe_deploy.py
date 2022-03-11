1c103472585b8a9baf3301e783e594d932a7b4cf
kaz <kaz>
2022-03-11 10:35:30 -0500

    examples

--- ble/bluepy/cc26x2r/recipe_deploy.py
+++ ble/bluepy/cc26x2r/recipe_deploy.py
@@ -2,11 +2,13 @@ from mat.ble.bluepy.cc26x2r_logger_controller import LoggerControllerCC26X2R
 from _macs import get_mac
 
 
-def deploy(c_d: dict, mac):
+def _deploy(c_d: dict, mac):
 
     lc = LoggerControllerCC26X2R(mac)
 
     if lc.open():
+        rv = lc.ble_cmd_sts()
+        print('> status: {}'.format(rv))
         rv = lc.ble_cmd_stp()
         print('> stop: {}'.format(rv))
         rv = lc.ble_cmd_stm()
@@ -15,8 +17,6 @@ def deploy(c_d: dict, mac):
         print('> get time: {}'.format(rv))
         rv = lc.ble_cmd_frm()
         print('> format: {}'.format(rv))
-        rv = lc.ble_cmd_sts()
-        print('> status: {}'.format(rv))
         rv = lc.ble_cmd_wli("SN1234567")
         print('> set info SN: {}'.format(rv))
         rv = lc.ble_cmd_wli("CA1234")
@@ -35,32 +35,42 @@ def deploy(c_d: dict, mac):
     lc.close()
 
 
-if __name__ == '__main__':
-    cfg_do = {
-        "DFN": "low",
+def cfg_do():
+    # mac = get_mac()
+    mac = '60:77:71:22:C8:49'
+
+    d = {
+        "DFN": "kaz",
         "TMP": 0, "PRS": 0,
         "DOS": 1, "DOP": 1, "DOT": 1,
-        "TRI": 10, "ORI": 10, "DRI": 30,
+        "TRI": 10, "ORI": 10, "DRI": 20,
         "PRR": 1,
         "PRN": 1,
         "STM": "2012-11-12 12:14:00",
         "ETM": "2030-11-12 12:14:20",
         "LED": 1
     }
-    cfg_mat = {
+    _deploy(d, mac)
+
+
+def cfg_mat():
+    mac = get_mac()
+
+    d = {
         "DFN": "low",
         "TMP": 1, "PRS": 0,
         "DOS": 0, "DOP": 0, "DOT": 0,
-        "TRI": 10, "ORI": 10, "DRI": 30,
+        "TRI": 10, "ORI": 10, "DRI": 20,
         "PRR": 1,
         "PRN": 1,
         "STM": "2012-11-12 12:14:00",
         "ETM": "2030-11-12 12:14:20",
         "LED": 1
     }
+    _deploy(d, mac)
 
-    # d = cfg_do
-    d = cfg_mat
-    list_of_macs = [get_mac(), ]
-    for each_mac in list_of_macs:
-        deploy(d, each_mac)
+
+if __name__ == '__main__':
+
+    cfg_do()
+    # cfg_mat()
